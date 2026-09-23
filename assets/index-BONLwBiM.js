646a9221d9b9146ca520e7da42afccced322e491
Windward Releases <releases@users.noreply.github.com>
2026-09-23 12:31:12 -0400

    web: 0.5.1-ota1 (90eec1f)

---
+++ assets/index-BONLwBiM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D6yor_Nb.js","./index-CBCTWygi.js","./index-FaHtIA08.css"])))=>i.map(i=>d[i]);
+import{t as r,_ as t}from"./index-CBCTWygi.js";const _=r("Share",{web:()=>t(()=>import("./web-D6yor_Nb.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
