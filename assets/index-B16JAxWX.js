333942eea5f84a79dfed282c4635a9c7333a666d
Windward Releases <releases@users.noreply.github.com>
2026-09-25 19:36:48 +0000

    web: 0.5.1-ota6 (f20756c)

--- assets/index-B16JAxWX.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DfkQHzIi.js","./index-CY6zD8Lp.js","./index-DkW4PmWw.css"])))=>i.map(i=>d[i]);
-import{F as r,_ as t}from"./index-CY6zD8Lp.js";const _=r("Share",{web:()=>t(()=>import("./web-DfkQHzIi.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
---
+++ assets/index-DB9C1CsG.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CT73WfM2.js","./index-DBtwqMzQ.js","./index-Bp36ujYI.css"])))=>i.map(i=>d[i]);
+import{F as r,_ as t}from"./index-DBtwqMzQ.js";const _=r("Share",{web:()=>t(()=>import("./web-CT73WfM2.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
